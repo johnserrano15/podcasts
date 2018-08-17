cdc86ba53666bb445dff6dfae3bf45cbfaccbb94
john andrey <andrey_201015@hotmail.com>
2018-08-17 09:57:53 -0500

    Puliendo detalles en los channel y trabajando el podcast

--- pages/channel.js
+++ pages/channel.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default class extends React.Component {
 
   static async getInitialProps({ query }) {
@@ -17,26 +19,47 @@ export default class extends React.Component {
 
     let dataSeries = await reqSeries.json()
     let series = dataSeries.body.channels
-    
+
     return { channel, audioClips, series }
   }
   render() {
     const { channel, audioClips, series } = this.props
     // console.log(channel)
     // console.log(series)
+    // console.log(audioClips)
     return <div>
       <header>Podcats</header>
 
+      <div className='banner' style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
+
       <h1>{ channel.title }</h1>
 
-      <h2>Series</h2>
-      { series.map((serie) => (
-        <div key={ serie.id }>{ serie.title }</div>
-      )) }
+      { series.length > 0 &&
+        <div>
+          <h2>Series</h2>
+          <div className='channels'>
+            { series.map((serie) => (
+              <Link href={`/channel?id=${ serie.id }`} prefetch key={serie.id}>
+                <a className='channel'>
+                  <img src={ serie.urls.logo_image.original } alt={serie.title}/>
+                  <h2>{ serie.title }</h2>
+                </a>
+              </Link>
+            )) }
+          </div>
+        </div>
+      }
 
-      <h2>Ultimos Podcats</h2>
+      <h2>Ultimos Podcasts</h2>
       { audioClips.map((clip) => (
-        <div key={ clip.id }>{ clip.title }</div>
+        <Link href={`/podcast?id=${clip.id}`} prefetch key={clip.id}>
+          <a className='podcast'>
+            <h3>{ clip.title }</h3>
+            <div className='meta'>
+              { Math.ceil(clip.duration / 60) } minutes
+            </div>
+          </a>
+        </Link>
       )) }
 
       <style jsx>{`
@@ -46,6 +69,15 @@ export default class extends React.Component {
           padding: 15px;
           text-align: center;
         }
+
+        .banner {
+          width: 100%;
+          padding-bottom: 25%;
+          background-position: 50% 50%;
+          background-size: cover;
+          background-color: #aaa;
+        }
+
         .channels {
           display: grid;
           grid-gap: 15px;
@@ -74,11 +106,33 @@ export default class extends React.Component {
           margin: 0;
           text-align: center;
         }
+
+        .podcast {
+          display: block;
+          text-decoration: none;
+          color: #333;
+          padding: 15px;
+          border-bottom: 1px solid rgba(0,0,0,0.2);
+          cursor: pointer;
+        }
+        .podcast:hover {
+          color: #000;
+        }
+        .podcast h3 {
+          margin: 0;
+        }
+        .podcast .meta {
+          color: #666;
+          margin-top: 0.5em;
+          font-size: 0.8em;
+        }
       `}</style>
+
       <style jsx global>{`
         body {
           margin: 0;
           font-family: system-ui;
+          background: white;
         }
       `}</style>
     </div>
---
+++ pages/podcast.js
@@ -0,0 +1,6 @@
+
+export default class extends React.Component {
+  render() {
+    return <h1>Hola mundo podcast</h1>
+  }
+}
